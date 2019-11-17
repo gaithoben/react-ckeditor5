b09ab6701ba265ad7801fe37c11b1408df585090
Bernard Gaitho <gaithoben@gmail.com>
2019-11-18 01:56:46 +0300

    fix(Editor): Update the editor and add new features to the editor
    Update CKEditor to latest verrsion 15.0.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from "react";
-import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
+import React from "react";
+import ClassicEditorBase from "@ckeditor/ckeditor5-editor-classic/src/classiceditor";
 
 import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
 import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
@@ -26,177 +26,203 @@ import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
 import HorizontalLine from "@ckeditor/ckeditor5-horizontal-line/src/horizontalline";
 import WordCount from "@ckeditor/ckeditor5-word-count/src/wordcount";
 
-import "./ckeditor.css";
+import ReactCKEditor from "@ckeditor/ckeditor5-react";
 
-// export class ClassicEditor extends ClassicEditorBase {}
+export class ClassicEditor extends ClassicEditorBase {}
 
-export default class CKEditor extends Component {
-  static defaultProps = {
-    uploadUrl: "/fileapi/upload/editorimage",
-    value: "<p>&nbsp;</p>",
-    input: {
-      value: "<p>&nbsp;</p>",
-      onChange: () => {},
-      onBlur: () => {}
+const CKEditor = ({
+  input,
+  value,
+  headers,
+  uploadUrl,
+  imageplugin,
+  headingplugin,
+  mediaplugin,
+  tableplugin,
+  onUpdate,
+  onFocus,
+  containerStyles
+}) => {
+  const val = input.value || value;
+
+  const [data, setData] = React.useState("");
+
+  const Editor = React.useRef(ClassicEditor).current;
+
+  const imageplugins = imageplugin
+    ? [EasyImage, Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload]
+    : [];
+
+  const mediaplugins = mediaplugin ? [MediaEmbed] : [];
+
+  const headingplugins = headingplugin ? [Heading] : [];
+
+  const tableplugins = tableplugin ? [Table, TableToolbar] : [];
+
+  const imagestoolbar = imageplugin
+    ? [
+        "insertimage",
+        "imageStyleAlignLeft",
+        "imageStyleFull",
+        "imageStyleAlignRight"
+      ]
+    : [];
+  const headingtoolbar = headingplugin ? ["headings"] : [];
+
+  Editor.builtinPlugins = [
+    Essentials,
+    Autoformat,
+    Bold,
+    Italic,
+    BlockQuote,
+    CKFinder,
+    ...headingplugins,
+    ...imageplugins,
+    ...mediaplugins,
+    SimpleUploadAdapter,
+    Indent,
+    Link,
+    List,
+    Paragraph,
+    ...tableplugins,
+    Alignment,
+    HorizontalLine,
+    WordCount
+  ];
+
+  Editor.defaultConfig = {
+    toolbar: {
+      items: [
+        ...(headingplugin ? ["heading", "|"] : []),
+        "bold",
+        "italic",
+        "link",
+        "bulletedList",
+        "numberedList",
+        "alignment",
+        "|",
+        "indent",
+        "outdent",
+        "|",
+        ...(imageplugin ? ["imageUpload"] : []),
+        ...imagestoolbar,
+        "blockQuote",
+        ...(tableplugin ? ["insertTable"] : []),
+        ...(mediaplugin ? ["mediaEmbed"] : []),
+        "undo",
+        "redo",
+        "horizontalLine"
+      ]
     },
-    meta: {},
+    image: imageplugin
+      ? {
+          toolbar: [
+            "imageTextAlternative",
+            "|",
+            "imageStyleAlignLeft",
+            "imageStyleFull",
+            "imageStyleAlignRight"
+          ],
+
+          styles: [
+            // This option is equal to a situation where no style is applied.
+            "imageStyleFull",
+
+            // This represents an image aligned to left.
+            "imageStyleAlignLeft",
+
+            // This represents an image aligned to right.
+            "imageStyleAlignRight"
+          ]
+        }
+      : {},
+    table: tableplugin
+      ? {
+          contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"]
+        }
+      : {},
+    simpleUpload: {
+      // The URL that the images are uploaded to.
+      uploadUrl: uploadUrl,
+
+      // Headers sent along with the XMLHttpRequest to the upload server.
+      headers: {
+        ...headers
+      }
+    },
+    wordCount: {
+      onUpdate: stats => {
+        // Prints the current content statistics.
+        onUpdate(stats);
+      }
+    },
+    // This value must be kept in sync with the language defined in webpack.config.js.
+    language: "en"
+  };
+
+  React.useEffect(() => {
+    if (val) {
+      setData(val);
+    }
+  }, [val]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flex: 1,
+        minHeight: "400px",
+        ...containerStyles
+      }}
+    >
+      <ReactCKEditor
+        editor={Editor}
+        data={data}
+        onInit={editor => {
+          // You can store the "editor" and use when it is needed.
+          console.log("Editor is ready to use!", editor);
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          if (typeof input.onChange === "function") {
+            input.onChange(data);
+          }
+          if (typeof onChange === "function") {
+            onChange(data);
+          }
+        }}
+        onBlur={(event, editor) => {
+          if (typeof input.onBlur === "function") {
+            input.onBlur();
+          }
+        }}
+        onFocus={(event, editor) => {
+          if (typeof onFocus === "function") {
+            onFocus();
+          }
+        }}
+      />
+    </div>
+  );
+};
+
+CKEditor.defaultProps = {
+  uploadUrl: "/fileapi/upload/editorimage",
+  value: "<p>&nbsp;</p>",
+  input: {
+    value: "<p>&nbsp;</p>",
     onChange: () => {},
-    imageplugin: false,
-    headingplugin: false
-  };
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    const val = `${nextProps.input.value || nextProps.value}`;
-    if (val !== prevState.defaultValue) {
-      return { ...prevState, defaultValue: val };
-    }
-
-    return { ...prevState };
-  }
-  constructor(props) {
-    super(props);
-    this.editor = null;
-    this.el = null;
-    this.state = {
-      defaultValue: "<p>&nbsp;</p>",
-      firstUpdate: false
-    };
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.editor && this.editor.setData && !this.state.firstUpdate) {
-      const editordata = this.editor.getData();
-      if (editordata !== this.state.defaultValue) {
-        this.editor.setData(this.state.defaultValue);
-        this.setState({ firstUpdate: true });
-      }
-    }
-  }
-
-  onChange(data) {
-    this.setState({ defaultValue: data }, () => {
-      this.props.input.onChange(data);
-      this.props.onChange(data);
-      this.props.input.onBlur();
-    });
-  }
-
-  onInitialized = () => {
-    if (this.editor && this.editor.setData && !this.state.firstUpdate) {
-      const editordata = this.editor.getData();
-      if (editordata !== this.state.defaultValue) {
-        this.editor.setData(this.state.defaultValue);
-        this.setState({ firstUpdate: true });
-      }
-    }
-
-    setTimeout(() => {
-      this.setState({ firstUpdate: true });
-    }, 1000);
-  };
-
-  componentWillUnmount() {
-    this.setState({ firstUpdate: false });
-  }
-  componentDidMount = () => {
-    ClassicEditor.create(this.el, {
-      plugins: [
-        Essentials,
-        Autoformat,
-        Bold,
-        Italic,
-        BlockQuote,
-        CKFinder,
-        EasyImage,
-        Heading,
-        Image,
-        ImageCaption,
-        ImageStyle,
-        ImageToolbar,
-        ImageUpload,
-        SimpleUploadAdapter,
-        Indent,
-        Link,
-        List,
-        MediaEmbed,
-        Paragraph,
-        Table,
-        TableToolbar,
-        Alignment,
-        HorizontalLine,
-        WordCount
-      ],
-      toolbar: {
-        items: [
-          "heading",
-          "|",
-          "bold",
-          "italic",
-          "link",
-          "bulletedList",
-          "numberedList",
-          "alignment",
-          "|",
-          "indent",
-          "outdent",
-          "|",
-          "imageUpload",
-          "blockQuote",
-          "insertTable",
-          "mediaEmbed",
-          "undo",
-          "redo",
-          "horizontalLine"
-        ]
-      },
-      image: {
-        toolbar: [
-          "imageStyle:full",
-          "imageStyle:side",
-          "|",
-          "imageTextAlternative"
-        ]
-      },
-      table: {
-        contentToolbar: ["tableColumn", "tableRow", "mergeTableCells"]
-      },
-      simpleUpload: {
-        // The URL that the images are uploaded to.
-        uploadUrl: this.props.uploadUrl,
-
-        // Headers sent along with the XMLHttpRequest to the upload server.
-        headers: {
-          ...this.props.headers
-        }
-      },
-      wordCount: {
-        onUpdate: stats => {
-          // Prints the current content statistics.
-          console.log(`Characters: ${stats.characters}\nWords: ${stats.words}`);
-        }
-      },
-      // This value must be kept in sync with the language defined in webpack.config.js.
-      language: "en"
-    })
-      .then(editor => {
-        console.log(editor);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
-
-  render() {
-    const { meta } = this.props;
-
-    return (
-      <div className="ck-editor-container">
-        <div
-          ref={el => {
-            this.el = el;
-          }}
-        />
-      </div>
-    );
-  }
-}
+    onBlur: () => {}
+  },
+  headers: {},
+  meta: {},
+  onChange: () => {},
+  onUpdate: () => {},
+  onFocus: () => {},
+  imageplugin: false,
+  headingplugin: false,
+  mediaplugin: false,
+  tableplugin: false,
+  containerStyles: {}
+};
+export default CKEditor;
